f617298e2bbba3fb20b1ad8617b35f3af1f42f7b
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-03-06 12:01:08 +0530

    Kaustav | #NOTAIGA | Added test cases for salary reducer. Reconfigure test runner. Added test coverage script

---
+++ src/reducers/tests/salary.reducer.test.js
@@ -0,0 +1,9 @@
+import { salaryReducer } from "../salary.reducer";
+
+describe('Salary Reducer', function () {
+	it('should provide unchanged state for default action', function () {
+		const state = { amount: 2000 };
+		const actualState = salaryReducer(state, "SOME_ACTION");
+		expect(actualState.amount).toEqual(2000);
+	});
+});
