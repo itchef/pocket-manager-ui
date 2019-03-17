e30e3259a8974141217187d17e18e079fd3e507c
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-03-17 23:40:12 +0530

    Kaustav | #NOTAIGA | Added typography and color combinations for default theme

--- src/theme/Default.theme.js
+++ src/theme/Default.theme.js
@@ -5,11 +5,18 @@ import { createMuiTheme } from "@material-ui/core";
 const defaultTheme = {
 	palette: {
 		primary: {
-			main: cyan[700]
+			main: cyan[700],
+			light: cyan[200],
+			dark: cyan[900]
 		},
 		secondary: {
-			main: lime[400]
+			main: lime[400],
+			light: lime[200],
+			dark: lime[700]
 		}
+	},
+	typography: {
+		useNextVariants: true
 	}
 };
 
